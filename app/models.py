02deddc988dbbd1b6b9d2871426b2a859f0084b4
ItsaPlant <krzysiekmichalik6@gmail.com>
2021-10-06 12:01:28 +0200

    2 commit

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from enum import unique
+from datetime import datetime
 from app import db
 
 class User(db.Model):
@@ -6,6 +6,16 @@ class User(db.Model):
     username = db.Column(db.String(100), index=True, unique=True)
     email = db.Column(db.String(200), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
 
     def __str__(self):
         return f"<User {self.username}>"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    created = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __str__(self):
+        return f"<Post {self.id} {self.body[:50]} ...>"
